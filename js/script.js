d20100191660e430f7be3471ce94b3aff0ce78d1
Alex Pham <alexpham095@gmail.com>
2018-10-04 16:37:37 -0700

    Button scripts finished. Pending redesign

--- js/script.js
+++ js/script.js
@@ -1,7 +1,124 @@
 $(document).ready(function() {
   let workTime = 1500;
+  let breakTime = 300;
+      currentTime = workTime,
+      displayPom = currentTime;
+  var stopped = true;
+  let onDisp = "Break";
 
   $("#displayClock").text(secToTimeString(workTime));
+
+  $("#decPom").click(function() {
+    if(displayPom == workTime && $("#displayPom").text() > 1){
+      let pom = $("#displayPom").text();
+      pom--;
+      $("#displayPom").text(pom);
+      displayPom = 
+        currentTime = 
+          workTime = eval(minToSec($("#displayPom").text()));
+      $("#displayClock").text(secToTimeString(workTime));
+    }
+  });
+
+  $("#incPom").click(function() {
+    if(displayPom == workTime){
+      let pom = $("#displayPom").text();
+      pom++;
+      $("#displayPom").text(pom);
+
+      displayPom = 
+        currentTime = 
+        workTime = eval(minToSec($("#displayPom").text()));
+      $("#displayClock").text(secToTimeString(workTime));
+    }
+  });
+
+  $("#decBreak").click(function() {
+    if(displayPom == workTime && $("#break").text() > 1){
+      let brk = $("#displayBreak").text();
+      brk--;
+      $("#displayBreak").text(brk);
+
+      breakTime = eval(minToSec($("#displayBreak").text()));
+    }
+  });
+
+  $("#incBreak").click(function(){
+    if(displayPom == workTime){
+      let brk = $("#displayBreak").text();
+      brk++;
+      $("#displayBreak").text(brk);
+
+      breakTime = eval(minToSec($("#displayBreak").text()));
+    }
+  });
+
+  $("#start").click(countdown);
+
+  $("#pause").click(pauseCountdown);
+
+  $("#stop").click(stopCountdown);
+  
+  $("#reset").click(resetCountdown);
+
+  function countdown(){
+    if(stopped === true){
+      displayPom = currentTime;
+      stopped = setInterval(decrement, 1000);
+    }
+    
+    function decrement(){
+      if(displayPom == 0){
+        $("#period").text(onDisp);
+        switch(onDisp){
+          case "Break":
+            displayPom = breakTime;
+            onDisp = "Work";
+            break;
+          case "Work":
+            displayPom = workTime;
+            onDisp = "Break";
+            break;
+        }
+        $("#displayClock").text(secToTimeString(displayPom));
+      } else {
+        displayPom--;
+        $("#displayClock").text(secToTimeString(displayPom));
+      }
+    }
+  }
+
+  function pauseCountdown(){
+    clearInterval(stopped);
+    stopped = true;
+    currentTime = displayPom;
+  }
+
+  function stopCountdown(){
+    clearInterval(stopped);
+    stopped = true;
+    displayPom = currentTime = workTime;
+    $("#displayClock").text(secToTimeString(displayPom));
+    $("#activity").text("Work");
+    onDisp = "Break";
+  }
+
+  function resetCountdown(){
+    clearInterval(stopped);
+    stopped = true;
+    displayPom = currentTime = workTime = minToSec(25);
+    breakTime = minToSec(5);
+
+    $("#displayClock").text(secToTimeString(displayPom));
+    $("#displayBreak").text(5);
+    $("#displayPom").text(25);
+    $("#activity").text("Work");
+    onDisp = "Break";
+  }
+
+  function minToSec(min){
+    return min*60;
+  }
 
   function secToTimeString(second){
     let hour = 0;
